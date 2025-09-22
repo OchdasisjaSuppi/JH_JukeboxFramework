1883c2c3d5b4cae2bf3112db1827e070e073ce68
OchdasisjaSuppi <08_15Gamer@posteo.at>
2025-09-22 15:58:13 +0200

    lua main & sound added

---
+++ main.lua
@@ -0,0 +1,152 @@
+-- JH_Jukebox_Framework_v1.0 by OchdasisjaSuppi
+
+-- nova.require "data\lua\core\common"
+
+--[[ 
+
+#
+### Define Music
+#
+
+# music_location
+#
+data\music\
+
+# music_register
+#
+data\lua\jh\sound\common.lua
+data\lua\jh\sound\music_callisto.lua
+data\lua\jh\sound\music_europa.lua
+data\lua\jh\sound\music_io.lua
+
+# music_use
+#
+data\lua\jh\data\generators\
+data\lua\jh\data\levels\
+data\lua\jh\data\worlds\
+
+
+#
+### Changed Music & Additions
+#
+
+Music Callisto	-----	-----	-----	-----	-----	-----	-----	-----	-----	-----
+	-- music_main_01
+	data\music		
+		jh_main.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_callisto_01
+	data\music\callisto\
+		jh_e1m1.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_callisto_02
+	data\music\callisto\
+		jh_e1m2.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_callisto_03
+	data\music\callisto\
+		jh_e1m3.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_callisto_04
+	data\music\callisto\
+		jh_e1m4.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_callisto_05
+    data\music\callisto\
+		the_tormented.ogg			| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_callisto_06
+    data\music\callisto\
+		silent_killer.ogg			| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+
+	-- music_callisto_07
+	data\music\callisto\
+		ztd3.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_cot_01
+    data\music\callisto\
+		blood2r5.ogg				| Note: This music file isn't used by the game.
+
+	-- music_callisto_intermission
+    data\music\callisto\
+		jh_e1i.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+-
+
+
+Music Europa	-----	-----	-----	-----	-----	-----	-----	-----	-----	-----
+
+	-- music_europa_01
+	data\music\europa\
+		ztd1.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_europa_02
+	data\music\europa\
+		alive_bitten_dead.ogg		| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_europa_03
+	data\music\europa\
+		ztd2.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_europa_04"
+	data\music\europa\
+		dying_alone.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_europa_05
+	data\music\europa\
+		survivor.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_europa_06
+	data\music\europa\
+		dawn2.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_europa_07
+	data\music\europa\
+		eclipsed.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_europa_intermission
+	data\music\europa\
+		jh_e2i.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+-
+
+
+Music Io ----	-----	-----	-----	-----	-----	-----	-----	-----	-----	-----
+	-- music "music_io_01
+	data\music\io\
+		only_after_dark.ogg			| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_io_02
+	data\music\io\
+		blood_2r1.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_io_03
+	data\music\io\
+		blood_2r6.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_io_04
+	data\music\io\
+		blood_2r2.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_io_intermission"
+	data\music\io\
+		jh_e3i.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+-
+
+
+Music Beyond	-----	-----	-----	-----	-----	-----	-----	-----	-----	-----
+	-- music_beyond_01
+	data\music\beyond\
+		jh_e1a1.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_beyond_02
+	data\music\beyond\
+		jh_e1a2.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_beyond_03
+	data\music\beyond\
+		jh_e1a3.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+
+	-- music_beyond_04
+	data\music\beyond\
+		jh_e1a4.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+--]] 
---
+++ sound.lua
@@ -0,0 +1,242 @@
+-- JH_Jukebox_Framework_v1.0 by OchdasisjaSuppi
+
+nova.require "data/lua/jh/sound/common"
+nova.require "data/lua/jh/sound/music_callisto"
+nova.require "data/lua/jh/sound/music_europa"
+nova.require "data/lua/jh/sound/music_io"
+
+
+register_music "music_main_01"
+{
+	-- main menu music 
+	-- The Arena | Limbo | Dante Station
+    group = "music",
+    "data/music/jh_main.ogg",
+}
+
+--[[
+register_music "music_intro_01"
+{
+	-- Audio for the intro video
+    group = "music",
+    "data/music/intro_audio_track.ogg",
+}
+--]]
+
+--
+-- Music Callisto --- ----- ----- ----- ----- -----
+--
+register_music "music_callisto_01"
+{
+    -- CALLISTO L1
+    group = "music",
+    -- volume = 0.6,
+    "data/music/callisto/jh_e1m1.ogg",
+}
+
+register_music "music_callisto_02"
+{
+    -- callisto default
+	-- Storage Room | Workshop | Callisto Hub
+    group = "music",
+    -- volume = 0.6,
+    "data/music/callisto/jh_e1m2.ogg",
+}
+
+register_music "music_callisto_03"
+{
+    -- callisto default
+	-- Storage Room | Workshop | Mimir | Valhalla
+    group = "music",
+    -- volume = 0.6,
+    "data/music/callisto/jh_e1m3.ogg",
+}
+
+register_music "music_callisto_04"
+{
+    -- callisto default
+	-- Storage Room | Workshop | Mimir | Valhalla
+    group = "music",
+    -- volume = 0.6,
+    "data/music/callisto/jh_e1m4.ogg",
+}
+
+register_music "music_callisto_05"
+{
+    -- callisto civilian and starport
+	-- Valhalla Spaceport
+    group = "music",
+    "data/music/callisto/the_tormented.ogg",
+}
+
+register_music "music_callisto_06"
+{
+    -- callisto caves
+	-- Laboratory | Strongroom | Rift
+    group = "music",
+    "data/music/callisto/silent_killer.ogg",
+}
+
+register_music "music_callisto_07"
+{
+    -- callisto command
+	-- Laboratory | Strongroom | Military Barracks | MDF Central | Rift | Valhalla Command
+    group = "music",
+    "data/music/callisto/ztd3.ogg",
+}
+
+register_music "music_cot_01"
+{
+    -- NOT USED IN GAME
+    group = "music",
+    "data/music/callisto/blood2r5.ogg",
+}
+
+register_music "music_callisto_intermission"
+{
+    -- callisto intermission screen
+    group = "music",
+    "data/music/callisto/jh_e1i.ogg",
+}
+
+--
+-- Music Europa ----- ----- ----- ----- ----- -----
+--
+register_music "music_europa_01"
+{
+    -- europa default
+	-- EUROPA L1 | Storage Room | Workshop 
+    group = "music",
+    "data/music/europa/ztd1.ogg",
+}
+
+register_music "music_europa_02"
+{
+	-- Laboratory | Strongroom | Biolabs | Containment Area
+    group = "music",
+    "data/music/europa/alive_bitten_dead.ogg",
+}
+
+register_music "music_europa_03"
+{
+	-- Tyre Outpost | Europa Concourse | Refueling Base
+    group = "music",
+    "data/music/europa/ztd2.ogg",
+}
+
+register_music "music_europa_04"
+{
+	-- Dig Zone 
+    group = "music",
+    "data/music/europa/dying_alone.ogg",
+}
+
+register_music "music_europa_05"
+{
+	-- Asterius | Asterius Breach
+    group = "music",
+    "data/music/europa/survivor.ogg",
+}
+
+register_music "music_europa_06"
+{
+	-- Ruins
+    group = "music",
+    "data/music/europa/dawn2.ogg",
+}
+
+register_music "music_europa_07"
+{
+	-- Frozen Temple
+    group = "music",
+    "data/music/europa/eclipsed.ogg",
+}
+
+register_music "music_europa_intermission"
+{
+    -- europa intermission screen
+    group = "music",
+    "data/music/europa/jh_e2i.ogg",
+}
+
+--
+-- Music Io --- ----- ----- ----- ----- ----- -----
+--
+register_music "music_io_01"
+{
+    -- io base
+	-- Io L1 | Io Nexus | Storage Room | Workshop | Infernal Lock 
+    group = "music",
+    "data/music/io/only_after_dark.ogg",
+}
+
+register_music "music_io_02"
+{
+    -- io refinery/caves
+	-- Io Blacksite | Io Halls | Io Mephitic
+    group = "music",
+    "data/music/io/blood_2r1.ogg",
+}
+
+register_music "music_io_03"
+{
+    -- io CRI labs
+	-- Black Site Vaults
+    group = "music",
+    "data/music/io/blood_2r6.ogg",
+}
+
+register_music "music_io_04"
+{
+    -- io cathedral
+	-- Dark Cathedral
+    group = "music",
+    "data/music/io/blood_2r2.ogg",
+}
+
+register_music "music_io_intermission"
+{
+    -- io intermission screen
+    group = "music",
+    "data/music/io/jh_e3i.ogg",
+}
+
+--
+-- Music Beyond ----- ----- ----- ----- ----- -----
+--
+register_music "music_beyond_01"
+{
+	-- Beyond Intro | Beyond | Beyond Pre Boss
+	-- Callisto Docking Bay  | The Rift
+	-- Dante Vestibule 		 | Dante Halls | Dante Colosseum | Dante Rafters | Dante Ossuary
+	-- The Pit (Europa)
+	-- Tutorial
+    group = "music",
+    "data/music/beyond/jh_e1a1.ogg",
+}
+
+register_music "music_beyond_02"
+{
+	-- Beyond 	   | Beyond Pre Boss
+	-- Dante Halls | Dante Colosseum | Dante Rafters | Dante Ossuary
+	-- Europa Central Dig 
+	-- Tutorial
+    group = "music",
+    "data/music/beyond/jh_e1a2.ogg",
+}
+
+register_music "music_beyond_03"
+{
+	-- Io Laboratory | Io Strongroom | CRI Labs | CRI Armory | Io Gateway
+    group = "music",
+    "data/music/beyond/jh_e1a3.ogg",
+}
+
+register_music "music_beyond_04"
+{
+	-- Beyond Crucible, The Shattered Abyss  | Beyond, Precipice of Defeat
+	-- Dante, Precipice of Defeat | Abattoir | Dante Inferno | Dante Altar
+	-- Io Warehouse | Noxious Hollow
+    group = "music",
+    "data/music/beyond/jh_e1a4.ogg",
+}
