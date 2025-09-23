c63ce021c9a4aab69bbf1b3c1d43ac4078b9211e
OchdasisjaSuppi <08_15Gamer@posteo.at>
2025-09-23 10:34:55 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -28,117 +28,118 @@ data\lua\jh\data\worlds\
 Music Callisto	-----	-----	-----	-----	-----	-----	-----	-----	-----	-----
 	-- music_main_01
 	data\music		
-		jh_main.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_main.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_callisto_01
 	data\music\callisto\
-		jh_e1m1.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1m1.ogg					| "SONG"		(ALBUM)			# Too loud, adjust volume in sound.lua
 
 	-- music_callisto_02
 	data\music\callisto\
-		jh_e1m2.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1m2.ogg					| "SONG"		(ALBUM)			# Too quiet, adjust volume in sound.lua
 
 	-- music_callisto_03
 	data\music\callisto\
-		jh_e1m3.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1m3.ogg					| "SONG"		(ALBUM)			# Not tested
 
 	-- music_callisto_04
 	data\music\callisto\
-		jh_e1m4.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1m4.ogg					| "SONG"		(ALBUM)			# Yada yada yada
 
 	-- music_callisto_05
     data\music\callisto\
-		the_tormented.ogg			| "SONG"		(ALBUM)			# 100% Working in 1.0
+		the_tormented.ogg			| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_callisto_06
     data\music\callisto\
-		silent_killer.ogg			| "SONG"		(ALBUM)			# 100% Working in 1.0
+		silent_killer.ogg			| "SONG"		(ALBUM)			# 100% Working
 
 
 	-- music_callisto_07
 	data\music\callisto\
-		ztd3.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		ztd3.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_cot_01
     data\music\callisto\
-		blood2r5.ogg				| Note: This music file isn't used by the game.
+		blood2r5.ogg				| Note: This music file is registered but not used in the game.
 
 	-- music_callisto_intermission
     data\music\callisto\
-		jh_e1i.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1i.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 
 Music Europa	-----	-----	-----	-----	-----	-----	-----	-----	-----	-----
 
 	-- music_europa_01
 	data\music\europa\
-		ztd1.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		ztd1.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_europa_02
 	data\music\europa\
-		alive_bitten_dead.ogg		| "SONG"		(ALBUM)			# 100% Working in 1.0
+		alive_bitten_dead.ogg		| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_europa_03
 	data\music\europa\
-		ztd2.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		ztd2.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_europa_04"
 	data\music\europa\
-		dying_alone.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+		dying_alone.ogg				| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_europa_05
 	data\music\europa\
-		survivor.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+		survivor.ogg				| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_europa_06
 	data\music\europa\
-		dawn2.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		dawn2.ogg					| "SONG"		(ALBUM)			# Jupiter Hell is awesome!
 
 	-- music_europa_07
 	data\music\europa\
-		eclipsed.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+		eclipsed.ogg				| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_europa_intermission
 	data\music\europa\
-		jh_e2i.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e2i.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 
 Music Io ----	-----	-----	-----	-----	-----	-----	-----	-----	-----	-----
 	-- music "music_io_01
 	data\music\io\
-		only_after_dark.ogg			| "SONG"		(ALBUM)			# 100% Working in 1.0
+		only_after_dark.ogg			| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_io_02
 	data\music\io\
-		blood_2r1.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+		blood_2r1.ogg				| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_io_03
 	data\music\io\
-		blood_2r6.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+		blood_2r6.ogg				| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_io_04
 	data\music\io\
-		blood_2r2.ogg				| "SONG"		(ALBUM)			# 100% Working in 1.0
+		blood_2r2.ogg				| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_io_intermission"
 	data\music\io\
-		jh_e3i.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e3i.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 
 Music Beyond	-----	-----	-----	-----	-----	-----	-----	-----	-----	-----
 	-- music_beyond_01
 	data\music\beyond\
-		jh_e1a1.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1a1.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_beyond_02
 	data\music\beyond\
-		jh_e1a2.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1a2.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_beyond_03
 	data\music\beyond\
-		jh_e1a3.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1a3.ogg					| "SONG"		(ALBUM)			# 100% Working
 
 	-- music_beyond_04
 	data\music\beyond\
-		jh_e1a4.ogg					| "SONG"		(ALBUM)			# 100% Working in 1.0
+		jh_e1a4.ogg					| "SONG"		(ALBUM)			# Have a nice day!
 --]] 
+
